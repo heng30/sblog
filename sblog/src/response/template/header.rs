ae0b26c3ddee29e87166c3f2b8d3bfc395bbc317
heng30 <2238288979@qq.com>
2025-02-02 22:47:38 +0800

    [*] update normally

--- sblog/src/response/template/header.rs
+++ sblog/src/response/template/header.rs
@@ -1,48 +1,66 @@
 pub const TEMPLATE: &str = r#"
-<header
-  style="
+<style>
+  .home-link {
+    font-size: 1.6em;
+    background: linear-gradient(to right, #eeeeee, #eeeeee) no-repeat right
+      bottom;
+    background-size: 0px 2px;
+    transition: background-size 800ms;
+  }
+
+  .home-link:hover {
+    background-position-x: left;
+    background-size: 100% 2px;
+    border-width: 0;
+  }
+
+  .header {
     height: 2.4em;
     display: $${{is-show-header}};
     justify-content: space-between;
-  "
->
-  <a href="/" style="font-size: 1.6em"> $${{site-name}} </a>
+  }
+
+  .form {
+    height: 2em;
+    width: 0;
+    margin-right: 8px;
+    transition: width 0.3s ease;
+    display: flex;
+    flex-direction: row;
+  }
+
+  .input {
+    width: 100%;
+    border-color: #aeaeae;
+    border-width: 0;
+    text-indent: 4px;
+    background-color: #202020;
+    color: #aeaeae;
+    outline-style: none;
+  }
+
+  .search {
+    display: flex;
+    margin: auto 8px;
+  }
+</style>
+
+<header class="header">
+  <a href="/" class="home-link"> $${{site-name}} </a>
 
   <div style="display: flex; flex-direction: row; margin-top: 0px">
-    <form
-      id="search-form"
-      action="/search"
-      style="
-        height: 2em;
-        width: 0;
-        margin-right: 8px;
-        transition: width 0.3s ease;
-        display: flex;
-        flex-direction: row;
-      "
-    >
+    <form id="search-form" action="/search" class="form">
       <input
         type="search"
         name="keyword"
         placeholder="搜索"
         required
-        style="
-          width: 100%;
-          border-color: #aeaeae;
-          border-width: 0;
-          text-indent: 4px;
-          background-color: #202020;
-          color: #aeaeae;
-          outline-style: none;
-        "
+        class="input"
         onblur="hideSearchForm()"
       />
     </form>
 
-    <a style="display: flex; margin: auto 8px" onclick="showSearchForm()"
-      >搜索</a
-    >
-
+    <a class="search" onclick="showSearchForm()">搜索</a>
     <a style="display: flex; margin: auto 8px" href="/about/">关于</a>
     <a style="display: flex; margin: auto 8px" href="/rss/">订阅</a>
   </div>
