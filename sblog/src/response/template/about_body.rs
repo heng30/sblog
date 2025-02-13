8f67487a507a00f3998a7a9e419b7a3249c25c52
blueJpg <2238288979@qq.com>
2025-02-13 18:15:42 +0800

    [+] 定期上传

--- sblog/src/response/template/about_body.rs
+++ sblog/src/response/template/about_body.rs
@@ -1,4 +1,18 @@
+
 pub const TEMPLATE: &str = r#"
+<style>
+  a {
+    text-decoration: none;
+    color: #eeeeee;
+    cursor: pointer;
+  }
+
+  a:hover {
+    border-bottom: 2px solid #eeeeee;
+    color: #eeeeee;
+  }
+</style>
+
 <div style="padding-top: 20px;">
     $${{about}}
 </div>
