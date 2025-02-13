e928a0f9cb3b28e33aa86c1e822252f13d78f714
blueJpg <2238288979@qq.com>
2025-02-13 18:22:28 +0800

    [+] 定期上传

--- sblog/src/response/template/about_body.rs
+++ sblog/src/response/template/about_body.rs
@@ -1,19 +1,22 @@
-
 pub const TEMPLATE: &str = r#"
 <style>
-  a {
+  .about {
+    padding-top: 1em;
+  }
+
+  .about a {
     text-decoration: none;
     color: #eeeeee;
     cursor: pointer;
   }
 
-  a:hover {
+  .about a:hover {
     border-bottom: 2px solid #eeeeee;
     color: #eeeeee;
   }
 </style>
 
-<div style="padding-top: 20px;">
+<div class="about" >
     $${{about}}
 </div>
 "#;
